9a4a7207fd576c216be2f04e26b73f055277b68d
dinnksis <dina.kepecheva@gmail.com>
2025-02-20 17:36:05 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int addd(int a,int b);
+int main() {
+    int a,b;
+    std::cin>>a>>b;
+    std::cout<<addd(a,b);
+}
---
+++ src/utils.cpp
@@ -0,0 +1,12 @@
+int addd(int a,int b){
+    return a+b;
+}
+
+/*int mult(int a,int b){
+    return a*b;
+}*/
+
+
+int subtr(int a,int b){
+    return a-b;
+}
