bcf78b26b37a5e6aea5efea572c456eed829cc92
dinnksis <dina.kepecheva@gmail.com>
2025-02-21 19:50:52 +0300

    fix: Update main output message in main branch

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@ int subtr(int a,int b);
 
 int main() {
     int a,b;
+    std::cout<<"введите 2 числа:";
     std::cin>>a>>b;
     std::cout<<"+:"<<addd(a,b)<<std::endl;
     std::cout<<"-:"<<subtr(a,b);
