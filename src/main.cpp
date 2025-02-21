35066e8261a262e59faaf917a476c885d7311c47
dinnksis <dina.kepecheva@gmail.com>
2025-02-21 19:14:48 +0300

    feat: Add multiplication function to utils

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 int addd(int a,int b);
 int subtr(int a,int b);
+int mult (int a,int b);
 
 int main() {
     int a,b;
     std::cin>>a>>b;
     std::cout<<"+:"<<addd(a,b)<<std::endl;
-    std::cout<<"-:"<<subtr(a,b);
+    std::cout<<"-:"<<subtr(a,b)<<std::endl;
+    std::cout<<"*:"<<mult(a,b);
 }
--- src/utils.cpp
+++ src/utils.cpp
@@ -2,11 +2,10 @@ int addd(int a,int b){
     return a+b;
 }
 
-/*int mult(int a,int b){
-    return a*b;
-}*/
-
-
 int subtr(int a,int b){
     return a-b;
+}
+
+int mult(int a,int b){
+    return a*b;
 }
